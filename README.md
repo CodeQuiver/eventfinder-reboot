ead8074e732e5d3aa681d40fe67581d6ff57919d
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-12-30 17:48:03 -0500

    added placeholder index files in routes and models folders

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # eventfinder-reboot
+
+Client-side view available via Heroku at https://aqueous-caverns-27627.herokuapp.com
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/mern",
+  {
+    useNewUrlParser: true
+    //added this line because error message said old URL parser is deprecated
+    //this is the line it instructed to add to use the new one
+  }
+);
 
 app.get('/', (req, res) => {
   res.send("Hello World!");
