13b4c75cbb769d50481baba030ce20fb2ec2cffe
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2019-01-04 15:24:33 -0500

    set up keys for Eventbrite and DarkSky in keys.js file and imported them to ApiRoutes.js

--- config/keys.js
+++ config/keys.js
@@ -1,18 +1,9 @@
-//TODO - replace example code below with Darksky and Eventbrite key references
-
-// console.log('this is loaded');
-
-// exports.twitter = {
-//     consumer_key: process.env.TWITTER_CONSUMER_KEY,
-//     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-//     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-//     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET};
-
-// exports.spotify = {
-//     id: process.env.SPOTIFY_ID,
-//     secret: process.env.SPOTIFY_SECRET
-//   };
+// console.log('keys.js is loaded');
 
 exports.darksky = {
-    secretkey: process.env.DARKSKY_SECRET
+    apikey: process.env.DARKSKY_SECRET
 };
+
+exports.eventbrite = {
+    apikey: process.env.EVENTBRITE_KEY
+}
--- routes/ApiRoutes.js
+++ routes/ApiRoutes.js
@@ -1,3 +1,13 @@
+/* FILE SUMMARY - This is the API call setup for the client-side. Using API.js from previous "eventfinder" app as model,
+this will have the calls to the actual outside APIs here. Adding the keys occurs at this stage, but will probably leave
+specifics of constructing the url on client-side such as the search parameters.
+So far the APIs being called are: Eventbrite, DarkSky
+
+Others may be added later to expand program features, such as additional event search engines.
+*/
+
+import axios from "axios"; //check if I need axios here or code it differently on the server-side
+
 // configuration - sets up .env file use
 require("../dotenv").config();
 
@@ -5,3 +15,8 @@ require("../dotenv").config();
 var keys = require("../config/keys.js");
 
 // keys can be referenced as follows (using spotify example) "keys.spotify.secret" referencing the keys.js file
+
+const EVENTBRITEBASEURL = "https://www.eventbriteapi.com/v3/events/search/?";
+const EVENTBRITEAPIKEY = keys.eventbrite.apikey;
+
+const WEATHAPIKEY = keys.darksky.apikey;
