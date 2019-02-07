f40528b7e32f689902e891d576cc32961cc00938
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2019-02-07 18:32:47 -0500

    mostly set up a simple button to attempt to call the api route, but console log test currently prints the entire promise object rather than Hello World, so need to fix my prop passing and event triggers

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import API from './utils/API.js';
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           
-          <p>
+          
           <h1>Coming Soon - User Interface!</h1>
+          <p>
             Work-in-progress. Completing back-end functionality first.<br />
             Thank you for visiting!
           </p>
+
+          <button onClick={API.eventSearch}>X</button>
           
         </header>
       </div>
